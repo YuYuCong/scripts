09c43a75b11f5f7b2626c2866294ecb0efc3b428
YuYuCong <windmillyucong@163.com>
2018-08-15 22:13:22 +0800

    uodate blogimgshot.sh

--- shell/blogimgshot.sh
+++ shell/blogimgshot.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-github_rqqepositories_url='https://github.com/YuYuCong/BlogImg.git'
+github_repositories_url='https://github.com/YuYuCong/BlogImg.git'
 github_user='YuYuCong'
-github_pwd=''
+github_pwd='不告诉你'
+
+if [ ! -d ~/bin ];then
+    mkdir ~/bin
+fi
+
+if [ ! -f ~/bin/blogimgshot.sh ];then
+cp -r blogimgshot.sh ~/bin
+fi
 
 if [ ! -d ~/Pictures/blogimg ]; then
     mkdir ~/Pictures/blogimg
@@ -12,13 +20,17 @@ cd ~/Pictures/blogimg
 
 if [ ! -d ./.git ];then
     git init
-    git remote add origin $github_rqqepositories_url
+    git remote add origin $github_repositories_url
 fi
 
 shutter -s -e -o '~/Pictures/blogimg/%y%m%d-%T.png'
 
+filename=`ls -t |awk 'NR==1 {print $NF}'`
+imgurl=${github_repositories_url%.git}/blob/master/${filename}
+echo $imgurl |xsel -i -b
+
 git add -A
-git commit -m "addnewimg"
+git commit -m "addimg"
 
 /usr/bin/expect <<EOF
 set timeout 20
@@ -28,16 +40,5 @@ send "$github_user\r"
 expect "Password" 
 send "$github_pwd\r"
 expect "set up"
-send "Done!"
+spawn echo "Done!"
 EOF
-
-
-
-
-
-
-
-
-
-
-
