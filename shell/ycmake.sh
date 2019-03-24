aada3e2c9d558df372454a108ea4c51f84d2cf9b
William Yu <windmillyucong@163.com>
2019-03-24 10:21:37 +0800

    Update ycmake.sh

--- shell/ycmake.sh
+++ shell/ycmake.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
 pwd
-
-#if [ -d ./build ];then
-#    rm -r ./build
-#fi
-#mkdir build
-
-if [ ! -d ./build ];then
+if [ -f CMakeLists.txt ];then
+    if [ ! -d ./build ];then
     mkdir ./build
+    fi
+
+    cd ./build
+    cmake ..
+    make
+    ls
+else
+echo "Can not find CMakeLists.txt"
 fi
-
-
-cd ./build
-cmake ..
-make
-ls
